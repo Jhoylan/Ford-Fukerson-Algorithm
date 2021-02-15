f9ddfa8b1d88882eea3bec2c0a76a697e8139311
Jhoylan Goncalves <jhoylangs@gmail.com>
2021-02-15 08:28:22 -0300

    first commit

---
+++ Algorithm.java
@@ -0,0 +1,322 @@
+package fordFukerson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Algorithm {
+	private List<List<Integer>> edges = new ArrayList<>();
+	private List<List<Integer>> flowsAndCapacities = new ArrayList<>();
+	private List<List<Integer>> path = new ArrayList<>();
+	private List<Integer> vertexes = new ArrayList<>();
+	private List<Boolean> visitsStatus = new ArrayList<>();
+	private List<Boolean> blockadesStatus = new ArrayList<>();
+	private int origin;
+	private int destiny;
+		
+	public int maxFlow() {
+		this.definingOriginAndDestiny();
+		this.iniatilizingVertexesAndTheirStatus();
+		
+		List<List<Integer>> originEdges = this.originEdges();
+		
+		for(int i = 0; i < originEdges.size(); i++) {
+			while(true) {
+				this.generatePath(originEdges.get(i), this.origin);
+				
+				if(this.itsAValidPath()) {
+					this.addFlow(this.bottleNeckFlow());					
+					this.cleanEverything();
+				}else if(this.path.size() > 1) {
+					this.cleanVisitsStatus();
+					this.cleanPath();
+				}else {
+					this.cleanEverything();
+					break;
+				}
+			}			
+		}
+		
+		return this.calculateMaxFlow();
+	}
+
+	public void addEdge(List<Integer> edge) {
+		edges.add(edge);
+		
+		List<Integer> flowAndCapacity = new ArrayList<>();
+		flowAndCapacity.add(0);
+		flowAndCapacity.add(0);
+		
+		this.flowsAndCapacities.add(flowAndCapacity);
+	}
+	
+	private void setFlow(int flow, List<Integer> edge) {
+		this.flowsAndCapacities.get(this.edges.indexOf(edge)).set(0, flow);
+	}
+	
+	public void setCapacity(int capacity, List<Integer> edge) {
+		this.flowsAndCapacities.get(this.edges.indexOf(edge)).set(1, capacity);
+	}
+
+	private void addFlow(int flow) {
+		int actualFlowOfOringEdge = this.getFlow(this.path.get(0));
+		this.setFlow(flow + actualFlowOfOringEdge, this.path.get(0));
+		int referenceVertex = 0;
+		boolean previousEdgeIsFoward = true;
+		
+		for(int i = 1; i < this.path.size(); i++) {
+			if(previousEdgeIsFoward) {
+				referenceVertex =  this.path.get(i - 1).get(1);
+			}else {
+				referenceVertex =  this.path.get(i - 1).get(0);
+			}
+			
+			boolean isFoward = this.isFoward(this.path.get(i), referenceVertex);
+			
+			int actualFlow = this.getFlow(this.path.get(i));
+			
+			if(isFoward) {
+				this.setFlow(actualFlow + flow, this.path.get(i));
+				referenceVertex =  this.path.get(i - 1).get(1);
+				previousEdgeIsFoward = true;
+			}else {
+				this.setFlow(actualFlow - flow, this.path.get(i));
+				referenceVertex = this.path.get(i - 1).get(0);
+				previousEdgeIsFoward = false;
+			}
+		}
+	}
+	
+	private int bottleNeckFlow() {
+		int bottleNeckFlow = this.getCapacity(this.path.get(0))	- this.getFlow(this.path.get(0));
+		int referenceVertex = 0;
+		boolean previousEdgeIsFoward = true;
+		
+		for(int i = 1; i < this.path.size(); i++) {
+			if(previousEdgeIsFoward) {
+				referenceVertex =  this.path.get(i - 1).get(1);
+			}else {
+				referenceVertex =  this.path.get(i - 1).get(0);
+			}
+			
+			boolean isFoward = this.isFoward(this.path.get(i), referenceVertex);
+			
+			int flow = this.getFlow(this.path.get(i));
+			
+			if(isFoward) {
+				int capacity = this.getCapacity(this.path.get(i));
+								
+				if(capacity - flow < bottleNeckFlow) {
+					bottleNeckFlow = capacity - flow;
+				}
+				
+				previousEdgeIsFoward = true;
+			}else {
+				if(flow < bottleNeckFlow) {
+					bottleNeckFlow = flow;
+				}
+				
+				previousEdgeIsFoward = false;
+			}
+			
+		}
+		
+		return bottleNeckFlow;	
+	}
+
+	private void generatePath(List<Integer> edge, int vertex){
+		int adjacentVertex = this.adjacentVertex(edge, vertex);
+		
+		this.visitsStatus.set(this.vertexes.indexOf(vertex), true);
+		this.visitsStatus.set(this.vertexes.indexOf(adjacentVertex), true);
+		
+		boolean edgeIsSaturated = this.isSaturated(edge, this.isFoward(edge, vertex));
+		
+		if(!edgeIsSaturated) {
+			this.path.add(edge);
+		}	
+		
+		if(!edgeIsSaturated && this.adjacentVertex(edge, vertex) != this.destiny) {
+						
+			List<List<Integer>> adjacentEdges = this.adjacentEdges(edge, vertex);
+			
+			boolean isCornered = true;
+			
+			for(int i = 0; i < adjacentEdges.size(); i++) {
+				if(this.itsAPossibleEdge(adjacentEdges.get(i), adjacentVertex)) {
+					this.generatePath(adjacentEdges.get(i), adjacentVertex);
+					isCornered = false;
+					break;
+				}
+			}
+			
+			if(isCornered) {
+				this.blockadesStatus.set(this.vertexes.indexOf(adjacentVertex), true);
+			}
+		}
+	} 
+	
+	private boolean itsAValidPath() {
+		if(this.path.size() > 0) {
+			return this.path.get(this.path.size() - 1).get(1) == this.destiny;			
+		}	
+		
+		return false;
+	}
+	
+	public boolean itsAPossibleEdge(List<Integer> edge, int vertex) {
+		return !this.isSaturated(edge, this.isFoward(edge, vertex)) 
+				&& !this.visitsStatus.get(this.vertexes.indexOf(this.adjacentVertex(edge, vertex)))
+				&& !this.blockadesStatus.get(this.vertexes.indexOf(this.adjacentVertex(edge, vertex)));
+	}
+	
+	private List<List<Integer>> adjacentEdges(List<Integer> edge, int vertex){
+		List<List<Integer>> adjacentEdges = new ArrayList<>();
+
+		int adjacentVertex = this.adjacentVertex(edge, vertex);
+		
+		for(int i = 0; i < this.edges.size(); i++) {
+			if(this.edges.get(i).indexOf(adjacentVertex) != -1 && !this.edges.get(i).equals(edge)) {
+				adjacentEdges.add(this.edges.get(i));
+			}
+		}
+		
+		return adjacentEdges;
+	}
+
+	private boolean isFoward(List<Integer> edge, int vertex) {
+		if(edge.get(0) == vertex) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+
+	private int adjacentVertex(List<Integer> edge, int vertex) {
+			
+		if(edge.get(0) == vertex) {
+			return edge.get(1);
+		}
+		
+		return edge.get(0);
+	}
+	
+	private void iniatilizingVertexesAndTheirStatus() {
+		for(int i = 0; i < this.edges.size(); i++) {
+			if(this.vertexes.indexOf(this.edges.get(i).get(0)) == -1) {
+				this.vertexes.add(this.edges.get(i).get(0));
+				this.visitsStatus.add(false);
+				this.blockadesStatus.add(false);
+			}
+			
+			if(this.vertexes.indexOf(this.edges.get(i).get(1)) == -1) {
+				this.vertexes.add(this.edges.get(i).get(1));
+				this.visitsStatus.add(false);
+				this.blockadesStatus.add(false);
+			}
+		}
+	}
+	
+	private boolean isSaturated(List<Integer> edge, boolean isFoward) {
+		int flow = this.getFlow(edge);
+		
+		if(isFoward) {
+			
+			int capacity = this.getCapacity(edge);
+			
+			if(capacity - flow == 0) {
+				return true;
+			}
+			
+			return false;
+		}else {
+					
+			if(flow == 0) {
+				return true;
+			}
+			
+			return false;
+		}
+	}
+
+	private void definingOriginAndDestiny() {
+		for(int i = 0; i < this.edges.size(); i++) {
+			boolean isOrigin = true;
+			boolean isDestiny = true;
+			
+			int possibleOrigin = this.edges.get(i).get(0);
+			int possibleDestiny = this.edges.get(i).get(1);
+			
+			for(int j = 0; j < this.edges.size(); j++) {
+				if(this.edges.get(j).get(1) == possibleOrigin) {
+					isOrigin = false;
+				}
+				
+				if(this.edges.get(j).get(0) == possibleDestiny) {
+					isDestiny = false;
+				}
+			}
+			
+			if(isOrigin) {
+				this.origin = possibleOrigin;
+			}
+			
+			if(isDestiny) {
+				this.destiny = possibleDestiny;
+			}
+		}
+	}
+	
+	private void cleanBlockades() {
+		for(int i = 0; i < this.blockadesStatus.size(); i++) {
+			this.blockadesStatus.set(i, false);
+		}
+	}
+	
+	private void cleanVisitsStatus() {
+		for(int i = 0; i < this.visitsStatus.size(); i++) {
+			this.visitsStatus.set(i, false);
+		}
+	}
+
+	private List<List<Integer>> originEdges(){
+		List<List<Integer>> originEdges = new ArrayList<>();
+		
+		for(int i = 0; i < this.edges.size(); i++) {
+			if(this.edges.get(i).get(0) == this.origin) {
+				originEdges.add(this.edges.get(i));
+			}
+		}
+		
+		return originEdges;
+	}
+	
+	private int calculateMaxFlow() {
+		List<List<Integer>> originEdges = this.originEdges();
+		
+		int maxFlow = 0;
+		
+		for(int i = 0; i < originEdges.size(); i++) {
+			maxFlow += this.flowsAndCapacities.get(this.edges.indexOf(originEdges.get(i))).get(0);
+		}
+		
+		return maxFlow;
+	}
+
+	private int getCapacity(List<Integer> edge) {
+		return this.flowsAndCapacities.get(this.edges.indexOf(edge)).get(1);
+	}
+	
+	private int getFlow(List<Integer> edge) {
+		return this.flowsAndCapacities.get(this.edges.indexOf(edge)).get(0);
+	}
+	
+	private void cleanPath() {
+		this.path = new ArrayList<>();
+	}
+
+	private void cleanEverything() {
+		this.cleanVisitsStatus();
+		this.cleanBlockades();
+		this.cleanPath();
+	}
+}
---
+++ AlgorithmTest.java
@@ -0,0 +1,246 @@
+package fordFukerson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class AlgorithmTest {
+	@Test
+	void maxFlowInternetTest() throws InterruptedException {
+		Algorithm fordFukerson = new Algorithm();
+		
+		List<Integer> edge1 = new ArrayList<>();
+		edge1.add(1);
+		edge1.add(2);
+		
+		List<Integer> edge2 = new ArrayList<>();
+		edge2.add(1);
+		edge2.add(3);
+		
+		List<Integer> edge3 = new ArrayList<>();
+		edge3.add(2);
+		edge3.add(3);
+		
+		List<Integer> edge4 = new ArrayList<>();
+		edge4.add(2);
+		edge4.add(4);
+		
+		List<Integer> edge5 = new ArrayList<>();
+		edge5.add(2);
+		edge5.add(5);
+		
+		List<Integer> edge6 = new ArrayList<>();
+		edge6.add(3);
+		edge6.add(5);
+		
+		List<Integer> edge7 = new ArrayList<>();
+		edge7.add(4);
+		edge7.add(6);
+		
+		List<Integer> edge8 = new ArrayList<>();
+		edge8.add(5);
+		edge8.add(4);
+		
+		List<Integer> edge9 = new ArrayList<>();
+		edge9.add(5);
+		edge9.add(6);
+		
+		fordFukerson.addEdge(edge1);
+		fordFukerson.addEdge(edge2);
+		fordFukerson.addEdge(edge3);
+		fordFukerson.addEdge(edge4);
+		fordFukerson.addEdge(edge5);
+		fordFukerson.addEdge(edge6);
+		fordFukerson.addEdge(edge7);
+		fordFukerson.addEdge(edge8);
+		fordFukerson.addEdge(edge9);
+		
+		fordFukerson.setCapacity(10, edge1);
+		fordFukerson.setCapacity(10, edge2);
+		fordFukerson.setCapacity(2, edge3);
+		fordFukerson.setCapacity(4, edge4);
+		fordFukerson.setCapacity(8, edge5);
+		fordFukerson.setCapacity(9, edge6);		
+		fordFukerson.setCapacity(10, edge7);
+		fordFukerson.setCapacity(6, edge8);
+		fordFukerson.setCapacity(10, edge9);
+		
+		Assertions.assertEquals(19, fordFukerson.maxFlow());
+	}
+	
+	@Test
+	void maxFlowGoldbargTest () {
+		Algorithm fordFukerson = new Algorithm();
+		
+		List<Integer> edge1 = new ArrayList<>();
+		edge1.add(1);
+		edge1.add(2);
+		
+		List<Integer> edge2 = new ArrayList<>();
+		edge2.add(1);
+		edge2.add(4);
+		
+		List<Integer> edge3 = new ArrayList<>();
+		edge3.add(1);
+		edge3.add(7);
+		
+		List<Integer> edge4 = new ArrayList<>();
+		edge4.add(2);
+		edge4.add(5);
+		
+		List<Integer> edge5 = new ArrayList<>();
+		edge5.add(3);
+		edge5.add(2);
+		
+		List<Integer> edge6 = new ArrayList<>();
+		edge6.add(3);
+		edge6.add(9);
+		
+		List<Integer> edge7 = new ArrayList<>();
+		edge7.add(4);
+		edge7.add(2);
+		
+		List<Integer> edge8 = new ArrayList<>();
+		edge8.add(4);
+		edge8.add(3);
+		
+		List<Integer> edge9 = new ArrayList<>();
+		edge9.add(4);
+		edge9.add(5);
+		
+		List<Integer> edge10 = new ArrayList<>();
+		edge10.add(5);
+		edge10.add(6);
+		
+		List<Integer> edge11 = new ArrayList<>();
+		edge11.add(5);
+		edge11.add(7);
+		
+		List<Integer> edge12 = new ArrayList<>();
+		edge12.add(6);
+		edge12.add(3);
+		
+		List<Integer> edge13 = new ArrayList<>();
+		edge13.add(6);
+		edge13.add(8);
+		
+		List<Integer> edge14 = new ArrayList<>();
+		edge14.add(6);
+		edge14.add(9);
+		
+		List<Integer> edge15 = new ArrayList<>();
+		edge15.add(7);
+		edge15.add(4);
+		
+		List<Integer> edge16 = new ArrayList<>();
+		edge16.add(7);
+		edge16.add(8);
+		
+		List<Integer> edge17 = new ArrayList<>();
+		edge17.add(8);
+		edge17.add(4);
+		
+		List<Integer> edge18 = new ArrayList<>();
+		edge18.add(8);
+		edge18.add(9);
+		
+		fordFukerson.addEdge(edge1);
+		fordFukerson.addEdge(edge2);
+		fordFukerson.addEdge(edge3);
+		fordFukerson.addEdge(edge4);
+		fordFukerson.addEdge(edge5);
+		fordFukerson.addEdge(edge6);
+		fordFukerson.addEdge(edge7);
+		fordFukerson.addEdge(edge8);
+		fordFukerson.addEdge(edge9);
+		fordFukerson.addEdge(edge10);
+		fordFukerson.addEdge(edge11);
+		fordFukerson.addEdge(edge12);
+		fordFukerson.addEdge(edge13);
+		fordFukerson.addEdge(edge14);
+		fordFukerson.addEdge(edge15);
+		fordFukerson.addEdge(edge16);
+		fordFukerson.addEdge(edge17);
+		fordFukerson.addEdge(edge18);
+		
+		fordFukerson.setCapacity(10, edge1);
+		fordFukerson.setCapacity(10, edge2);
+		fordFukerson.setCapacity(10, edge3);
+		fordFukerson.setCapacity(12, edge4);
+		fordFukerson.setCapacity(15, edge5);		
+		fordFukerson.setCapacity(10, edge6);
+		fordFukerson.setCapacity(8, edge7);
+		fordFukerson.setCapacity(10, edge8);
+		fordFukerson.setCapacity(10, edge9);
+		fordFukerson.setCapacity(20, edge10);
+		fordFukerson.setCapacity(10, edge11);
+		fordFukerson.setCapacity(10, edge12);
+		fordFukerson.setCapacity(10, edge13);
+		fordFukerson.setCapacity(7, edge14);		
+		fordFukerson.setCapacity(10, edge15);
+		fordFukerson.setCapacity(10, edge16);
+		fordFukerson.setCapacity(10, edge17);
+		fordFukerson.setCapacity(13, edge18);
+		
+		Assertions.assertEquals(30, fordFukerson.maxFlow());
+	}
+	
+	@Test
+	void hopelessPathTest() {
+		Algorithm fordFukerson = new Algorithm();
+		
+		List<Integer> edge1 = new ArrayList<>();
+		edge1.add(1);
+		edge1.add(2);
+		
+		List<Integer> edge2 = new ArrayList<>();
+		edge2.add(1);
+		edge2.add(3);
+		
+		List<Integer> edge3 = new ArrayList<>();
+		edge3.add(1);
+		edge3.add(6);
+		
+		List<Integer> edge4 = new ArrayList<>();
+		edge4.add(2);
+		edge4.add(7);
+		
+		List<Integer> edge5 = new ArrayList<>();
+		edge5.add(3);
+		edge5.add(4);
+		
+		List<Integer> edge6 = new ArrayList<>();
+		edge6.add(4);
+		edge6.add(5);
+		
+		List<Integer> edge7 = new ArrayList<>();
+		edge7.add(5);
+		edge7.add(2);
+		
+		List<Integer> edge8 = new ArrayList<>();
+		edge8.add(6);
+		edge8.add(7);
+		
+		fordFukerson.addEdge(edge1);
+		fordFukerson.addEdge(edge2);
+		fordFukerson.addEdge(edge3);
+		fordFukerson.addEdge(edge4);
+		fordFukerson.addEdge(edge5);
+		fordFukerson.addEdge(edge6);
+		fordFukerson.addEdge(edge7);
+		fordFukerson.addEdge(edge8);
+		
+		fordFukerson.setCapacity(10, edge1);
+		fordFukerson.setCapacity(10, edge2);
+		fordFukerson.setCapacity(10, edge3);
+		fordFukerson.setCapacity(10, edge4);
+		fordFukerson.setCapacity(10, edge5);		
+		fordFukerson.setCapacity(10, edge6);
+		fordFukerson.setCapacity(10, edge7);
+		fordFukerson.setCapacity(10, edge8);
+		
+		Assertions.assertEquals(20, fordFukerson.maxFlow());
+	}
+}
